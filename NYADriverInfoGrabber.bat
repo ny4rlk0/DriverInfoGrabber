16ad4c772d609a194b68391c5cfa50ea69b66103
ny4rlk0 <55476474+ny4rlk0@users.noreply.github.com>
2021-12-02 23:05:12 +0300

    Update NYADriverInfoGrabber.bat

--- NYADriverInfoGrabber.bat
+++ NYADriverInfoGrabber.bat
@@ -1,8 +1,25 @@
-@echo off
 Title NYA Driver Modeli Bulma Yazilimi
+color a
+@echo off
+cls
+if _%1_==_payload_  goto :payload
+
+:getadmin
+    echo %~nx0: Getting Administrator permissions from operating system.
+    set vbs=%temp%\getadmin.vbs
+    echo Set UAC = CreateObject^("Shell.Application"^)                >> "%vbs%"
+    echo UAC.ShellExecute "%~s0", "payload %~sdp0 %*", "", "runas", 1 >> "%vbs%"
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+goto :eof
+
+:payload
+
+::TheMagic::
 Mode con cols=80 lines=3
 cls & color 0A & echo.
 Set "dosya=DeviceManager.txt"
 echo       Dosya olusturulurken lutfen biraz bekleyin %dosya% ..... (File is creating... Please wait...)
 start /wait msinfo32 /report %dosya%
 start "" notepad %dosya%
+::TheMagic::
